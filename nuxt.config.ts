bdb914ca7b2790aa0ad92c8a6bf94ba2f8d3bda2
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-15 10:53:54 -0300

    Update devtools and add modules and CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss", '@vueuse/nuxt'],
+  css: ["@/assets/css/main.css"],
 })
