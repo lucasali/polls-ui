1235f2ee330562827067b8481fcacd798887143e
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-15 10:55:10 -0300

    Add Poll component

---
+++ components/Poll.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+interface Props {
+  title: string
+}
+
+const { title } = defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex w-96 flex-col gap-2">
+    <h2 class="text-balance">{{ title }}</h2>
+
+    <div class="flex flex-col gap-1">
+      <slot></slot>
+    </div>
+  </div>
+</template>
