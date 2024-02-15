5a6ab046d9b951bba6ae04551a2fa8c86703a5fb
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-15 10:53:49 -0300

    Refactor app.vue layout and footer

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <header></header>
-  <RouterView></RouterView>
-  <footer>© 2024</footer>
+  <main class="grid h-[calc(100dvh-24px)] place-items-center p-8">
+    <NuxtPage></NuxtPage>
+  </main>
+
+  <footer class="flex justify-end px-4">© 2024 - lucasali</footer>
 </template>
+
+<script lang="ts" setup>
+useHead({
+  titleTemplate: title => {
+    return title ? `Poll • ${title}` : 'Polls'
+  }
+})
+</script>
