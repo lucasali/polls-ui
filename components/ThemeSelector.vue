1586a55074f6b1ccc1b884afe871576fed5c36df
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-16 22:51:48 -0300

    Add ThemeSelector component

---
+++ components/ThemeSelector.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const colorMode = useColorMode()
+</script>
+
+<template>
+  <div>
+    <select v-model="colorMode.preference" class="bg-background h-8 w-24 rounded-md border-2">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </div>
+</template>
