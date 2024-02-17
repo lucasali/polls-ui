157a2ecf860c42fca791d233d5b33cf3b1e1172b
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-16 22:51:36 -0300

    Refactor Poll component to handle voting functionality

--- components/Poll.vue
+++ components/Poll.vue
@@ -1,17 +1,55 @@
 <script lang="ts" setup>
-interface Props {
+interface PollOption {
+  id: string
   title: string
+  votes: number
 }
 
-const { title } = defineProps<Props>()
+interface Poll {
+  id: string
+  title: string
+  options: PollOption[]
+}
+
+interface Props {
+  poll: Poll
+}
+
+const { poll } = defineProps<Props>()
+
+const votedOption = ref<string | null>(null)
+
+function handleFormSubmit(e: Event) {
+  e.preventDefault()
+
+  $fetch(`http://localhost:3333/polls/${poll.id}/votes`, {
+    method: 'POST',
+    body: { pollOptionId: votedOption.value }
+  })
+}
+
+function handleVote(optionId: string) {
+  votedOption.value = optionId
+}
 </script>
 
 <template>
-  <div class="flex w-96 flex-col gap-2">
-    <h2 class="text-balance">{{ title }}</h2>
+  <form @submit="handleFormSubmit">
+    <div class="flex w-96 flex-col gap-4">
+      <h2 class="text-balance">{{ poll.title }}</h2>
 
-    <div class="flex flex-col gap-1">
-      <slot></slot>
+      <div class="flex flex-col gap-2">
+        <template v-for="(option, i) in poll.options" :key="option.id">
+          <PollOption :option="option" :index="i" @vote="handleVote">{{ option.title }}</PollOption>
+        </template>
+      </div>
+
+      <button
+        type="submit"
+        class="rounded-md bg-primary p-4 uppercase text-white focus:outline-none focus:ring focus:ring-primary"
+      >
+        Enviar
+      </button>
     </div>
-  </div>
+  </form>
 </template>
