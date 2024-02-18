ec30db680360508b085cc95218f4c101942ceeee
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2024-02-18 10:51:12 -0300

    Refactor app.vue header and add logo

--- app.vue
+++ app.vue
@@ -1,5 +1,32 @@
+<script lang="ts" setup>
+useHead({
+  titleTemplate: title => {
+    return title ? `Polls • ${title}` : 'Polls'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/Polls Logo - Light.svg'
+    }
+  ]
+})
+
+const colorMode = useColorMode()
+
+const logo = computed(() => {
+  return colorMode.value === 'dark' ? '/Polls Logo - Light.svg' : '/Polls Logo - Dark.svg'
+})
+</script>
+
 <template>
-  <header class="flex justify-end p-2">
+  <header class="flex items-center justify-between px-8 py-2">
+    <NuxtLink to="/">
+      <div class="flex items-center gap-3">
+        <img :src="logo" alt="Polls Logo" class="size-8" />
+        <h1 class="text-2xl font-bold">Polls</h1>
+      </div>
+    </NuxtLink>
     <ThemeSelector></ThemeSelector>
   </header>
 
@@ -9,18 +36,3 @@
 
   <footer class="flex justify-end px-4">© 2024 - lucasali</footer>
 </template>
-
-<script lang="ts" setup>
-useHead({
-  titleTemplate: title => {
-    return title ? `Polls • ${title}` : 'Polls'
-  },
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/vnd.microsoft.icon',
-      href: '/favicon-dark.ico'
-    }
-  ]
-})
-</script>
